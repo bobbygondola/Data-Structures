463b4fca5e79c312c2144a4408bceeda2339389a
bobbygondola <60513315+bobbygondola@users.noreply.github.com>
2020-07-25 13:57:10 -0400

    notes

--- whiteboard.py
+++ whiteboard.py
@@ -3,7 +3,7 @@
 array = ["Joe", "2", "Ted", "4.98", "14", "Sam", "void *", "42", "float", "pointers", "5006"]
 
 # You may use whatever programming language you'd like.
-
+# REVIEW THIS IF YOU WANT TO GET A JOB WORKING WITH ELON MUSK
 # Verbalize your thought process as much as possible before writing any code. Run through the UPER problem solving framework while going through your thought process.
 
 for i in array:
